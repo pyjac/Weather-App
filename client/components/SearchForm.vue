1509272796808a36889d3ec9c77b8d80e6fe19b9
pyjac <oyebanji05@gmail.com>
2018-06-23 15:24:39 +0100

    Add SearchForm and Weather Component

---
+++ client/components/SearchForm.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="searchForm" class="form-inline align-self-center">
+    <div class="form-group mx-sm-3 mb-2">
+      <label for="inputPassword2" class="sr-only">Search </label>
+      <input type="text" class="form-control" id="search" v-model="searchText" placeholder="Search Location Weather">
+    </div>
+    <button type="submit" class="btn btn-primary mb-2" @click="searchLocationWeather" >Search</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      searchText: '',
+    }
+  },
+  methods: {
+    searchLocationWeather () {
+      this.onSearch(this.searchText)
+    }
+  },
+  props: ['onSearch'],
+}
+</script>
+
+<style>
+#searchForm {
+  margin: 0 auto;
+}
+</style>
+
---
+++ client/components/Weather.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="card">
+    <img class="card-img-top" :src="data.weatherIcon" alt="Card image cap">
+    <div class="card-body text-center">
+      <h5 class="card-title">{{ data.cityName }}</h5>
+      <h2>{{ data.temperature }}°C</h2>
+      <p>Max: {{ data.maxTemperature}}°C</p>
+      <p>Min: {{ data.minTemperature}}°C</p>
+    </div>
+  </div>
+</template>
+
+
+<script>
+
+export default {
+  props: ['data']
+}
+</script>
+<style>
+.card {
+  border: none;
+  width: 13rem;
+  margin: 0 auto;
+}
+</style>
+
