8e659aa040705bf636ec03fc05cb9dca81da55b6
pyjac <oyebanji05@gmail.com>
2018-06-23 15:31:34 +0100

    Add Home View and store values

--- client/store/index.js
+++ client/store/index.js
@@ -1,31 +1,57 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import WeatherResource from '../resources/weather'
 
 Vue.use(Vuex)
 
+
+// mutation-types
+const SET_WEATHER_DATA = 'SET_WEATHER_DATA';
+const SET_LOADING = 'SET_LOADING';
+
 const state = {
-  count: 0
+  homeWeatherData: [],
+  loading: false,
 }
 
 const mutations = {
-  INCREMENT (state) {
-    state.count++
+  SET_WEATHER_DATA (state, weatherData) {
+    Vue.set(state, 'homeWeatherData', weatherData)
   },
-  DECREMENT (state) {
-    state.count--
+  SET_LOADING (state, loading) {
+    Vue.set(state, 'loading', loading)
   }
 }
 
 const actions = {
-  incrementAsync ({ commit }) {
-    setTimeout(() => {
-      commit('INCREMENT')
-    }, 200)
-  }
+  async fetchHomeWeather ({ commit }) {
+    commit(SET_LOADING, true)
+    try {
+      const data = await Promise.all([
+        WeatherResource.getLocationWeather('2344116'),
+        WeatherResource.getLocationWeather('638242'),
+        WeatherResource.getLocationWeather('44418'),
+        WeatherResource.getLocationWeather('565346'),
+        WeatherResource.getLocationWeather('560743'),
+        WeatherResource.getLocationWeather('9807')
+      ])
+      commit(SET_WEATHER_DATA, data)
+    } catch (error) {
+      console.log(error)
+    }
+    commit(SET_LOADING, false)
+  },
+}
+
+// getters
+const getters = {
+  homeWeatherData: (state) => state.homeWeatherData,
+  loading: (state) => state.loading,
 }
 
 const store = new Vuex.Store({
   state,
+  getters,
   mutations,
   actions
 })
--- client/views/Home.vue
+++ client/views/Home.vue
@@ -1,18 +1,45 @@
 <template>
   <div class="page">
-    <counter></counter>
-    <p>
-      To get started, edit files in <code>./client</code> and save.
-    </p>
+    <div class="row">
+      <div class="col-sm-4 col-md-4" v-for="weather in weatherData">
+        <weather :data="weather" />
+      </div>
+    </div>
+    <div class="row">
+      <search-form :onSearch="searchLocationWeather" />
+    </div>
   </div>
 </template>
 
 <script>
-import Counter from 'components/Counter'
+import Weather from "components/Weather";
+import SearchForm from "components/SearchForm";
+import { mapActions, mapGetters } from "vuex";
 
 export default {
   components: {
-    Counter
+    Weather,
+    SearchForm
+  },
+  computed: {
+    ...mapGetters({
+      weatherData: "homeWeatherData"
+    })
+  },
+  methods: {
+    searchLocationWeather (searchText) {
+      this.$router.push({ name: 'search', params: { keyword: searchText}})
+    },
+    ...mapActions(["fetchHomeWeather"])
+  },
+  created: function() {
+    return this.fetchHomeWeather();
   }
-}
+};
 </script>
+
+<style>
+#searchForm {
+  margin: 0 auto;
+}
+</style>
